83374f13545c582a192b51aad31323c4457e6ab9
Sara <sara.zanellato21@gmail.com>
2019-10-29 22:32:08 +0100

    Changes in cart model

--- sarazon-api-node/models/cart.js
+++ sarazon-api-node/models/cart.js
@@ -1,29 +1,55 @@
 const mongoose = require("mongoose");
 const Joi = require("@hapi/joi");
-const { userSchema, joiUserSchema } = require("./user");
-const { productSchema, joiProductSchema } = require("./product");
+const { userSchema } = require("./user");
 
-const cartSchema = new mongoose.Schema({
-  user: {
-    type: userSchema,
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
     required: true
   },
-  products: {
-    type: [productSchema],
+  price: {
+    type: Number,
+    min: 3,
     required: true
+  },
+  // IMAGE
+  numberInStock: { type: Number, min: 0, required: true },
+  selectedQuantity: {
+    type: Number,
+    // MAX IS EQUAL TO NUMBERINSTOCK
+    min: 1,
+    default: 1
   }
+});
+
+const cartSchema = new mongoose.Schema({
+  user: { type: userSchema },
+  products: [
+    {
+      type: productSchema,
+      required: true
+    }
+  ]
 });
 
 const Cart = mongoose.model("Cart", cartSchema);
 
 const joiCartSchema = Joi.object({
-  user: joiUserSchema,
+  userId: Joi.objectId().required(),
   products: Joi.array()
-    .items(joiProductSchema)
+    .items(
+      Joi.object({
+        _id: Joi.objectId().required(),
+        selectedQuantity: Joi.number()
+          .min(1)
+          .required()
+      })
+    )
     .required()
 });
 
-const validate = (cart = joiCartSchema.validate(cart));
+const validate = cart => joiCartSchema.validate(cart);
 
 exports.Cart = Cart;
 exports.validate = validate;
