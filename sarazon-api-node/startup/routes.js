333bde7a58f000204571cc5c4e545a8dfb636905
Sara <sara.zanellato21@gmail.com>
2019-10-29 22:34:03 +0100

    Added cart route

--- sarazon-api-node/startup/routes.js
+++ sarazon-api-node/startup/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const products = require("../routes/products");
 const categories = require("../routes/categories");
 const users = require("../routes/users");
+const carts = require("../routes/carts");
 const auth = require("../routes/auth");
 const error = require("../middleware/error");
 
@@ -9,6 +10,7 @@ module.exports = function(app) {
   app.use(express.json());
   app.use("/api/products", products);
   app.use("/api/categories", categories);
+  app.use("/api/carts", carts);
   app.use("/api/users", users);
   app.use("/api/auth", auth);
   app.use(error);
