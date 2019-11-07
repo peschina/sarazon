044af777035f7fad8f04b5926378893ff7ba557e
Sara <sara.zanellato21@gmail.com>
2019-11-07 23:01:08 +0100

    http service module
    for abstracting axios

---
+++ sarazon-react/src/services/httpServices.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+//import logger from "./logService";
+
+axios.interceptors.response.use(null, error => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
+  if (!expectedError) {
+    console.log(error);
+    //logger.log(error);
+    //toast.error("un unexpected error occured");
+  }
+  return Promise.reject(error);
+});
+
+function setJwt(jwt) {
+  axios.defaults.headers.common["x-auth-token"] = jwt;
+}
+
+export default {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  delete: axios.delete
+};
