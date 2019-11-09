a2a4425afbbffca583eb9bcebb81db99dbb780c8
Sara <sara.zanellato21@gmail.com>
2019-11-09 17:31:57 +0100

    Protected routes
    Routes related to user are no longer accessible if user is not authenticated

--- sarazon-react/src/App.js
+++ sarazon-react/src/App.js
@@ -31,11 +31,29 @@ const App = () => {
           <Route path="/product/:id" component={ProductPage} />
           <Route path="/products" component={Products} />
           <Route path="/cart" component={Cart} />
-          <Route path="/wishlist" component={Wishlist} />
-          <Route path="/orders" component={Orders} />
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
-          <Route path="/profile" component={Profile} />
+          <Route
+            path="/profile"
+            render={props => {
+              if (!user) return <Redirect to="/login" />;
+              return <Profile />;
+            }}
+          />
+          <Route
+            path="/orders"
+            render={props => {
+              if (!user) return <Redirect to="/login" />;
+              return <Orders />;
+            }}
+          />
+          <Route
+            path="/wishlist"
+            render={props => {
+              if (!user) return <Redirect to="/login" />;
+              return <Wishlist />;
+            }}
+          />
           <Route path="/logout" component={Logout} />
           <Route path="/contact" component={Contact} />
           <Route path="/not-found" component={NotFound} />
