02d716c607955fc7fb552054744a84a2b5a7706e
Sara <sara.zanellato21@gmail.com>
2019-11-21 06:56:28 +0100

    Integration tests for POST handler in categories route

--- sarazon-api-node/test/integration/categories.test.js
+++ sarazon-api-node/test/integration/categories.test.js
@@ -1,11 +1,12 @@
 const request = require("supertest");
-const { Category } = require("../../../models/category");
+const { Category } = require("../../models/category");
+const { User } = require("../../models/user");
 
 let server;
 
 describe("/api/categories", () => {
   beforeEach(() => {
-    server = require("../../../index");
+    server = require("../../index");
   });
   afterEach(async () => {
     server.close();
@@ -44,7 +45,50 @@ describe("/api/categories", () => {
       await category.save();
       const res = await request(server).get(`/api/categories/${category._id}`);
       expect(res.status).toBe(200);
-      +expect(res.body).toHaveProperty("name", category.name);
+      expect(res.body).toHaveProperty("name", category.name);
+    });
+
+    describe("POST /", () => {
+      it("should return 401 if client is not logged in", async () => {
+        const res = await request(server)
+          .post("/api/categories")
+          .send({ name: "genre1" });
+        expect(res.status).toBe(401);
+      });
+
+      it("should return 400 if input is less than 3 characters", async () => {
+        const token = new User({ isAdmin: true }).generateAuthToken();
+
+        const res = await request(server)
+          .post("/api/categories")
+          .set("x-auth-token", token)
+          .send({ name: "a" });
+        expect(res.status).toBe(400);
+      });
+
+      it("should return 400 if input is less more than 50 characters", async () => {
+        const token = new User({ isAdmin: true }).generateAuthToken();
+
+        const name = new Array(52).join("a");
+
+        const res = await request(server)
+          .post("/api/categories")
+          .set("x-auth-token", token)
+          .send({ name });
+        expect(res.status).toBe(400);
+      });
+
+      it("should return 200 if input is valid", async () => {
+        const token = new User({ isAdmin: true }).generateAuthToken();
+
+        const res = await request(server)
+          .post("/api/categories")
+          .set("x-auth-token", token)
+          .send({ name: "genre1" });
+        expect(res.status).toBe(200);
+        expect(res.body).toHaveProperty("_id");
+        expect(res.body).toHaveProperty("name", "genre1");
+      });
     });
   });
 });
