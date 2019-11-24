ae2b8f60ee7922200f136ae4592f775cb5c31e85
Sara <sara.zanellato21@gmail.com>
2019-11-24 14:34:56 +0100

    Fix bug in login

--- sarazon-api-node/routes/auth.js
+++ sarazon-api-node/routes/auth.js
@@ -22,7 +22,7 @@ router.post("/", async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
   const user = await User.findOne({ email: req.body.email });
   if (!user) return res.status(400).send("Invalid email or password");
-  const validPassword = bcrypt.compare(req.body.password, user.password);
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send("Invalid email or password");
   const token = user.generateAuthToken();
   res.send(token);
