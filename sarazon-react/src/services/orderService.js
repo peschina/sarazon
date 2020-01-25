8c048c335851b44a4b9bd10edf3020f3fca3ecdd
Sara <sara.zanellato21@gmail.com>
2020-01-25 11:52:02 +0100

    Add service to add new order

--- sarazon-react/src/services/orderService.js
+++ sarazon-react/src/services/orderService.js
@@ -1,6 +1,11 @@
 import http from "./httpServices";
 import { apiEndpoint } from "../config.json";
+const apiUrl = `${apiEndpoint}/orders`;
 
 export function getOrders() {
-  return http.get(`${apiEndpoint}/orders`);
+  return http.get(apiUrl);
+}
+
+export function addOrder(order) {
+  return http.post(apiUrl, order);
 }
